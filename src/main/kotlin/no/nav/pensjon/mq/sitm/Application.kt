b1f1034888a2cee573ff4c052a81992b2bbb3562
Per Christian Moen <per.christian.moen@nav.no>
2024-09-19 20:41:17 +0200

    Generert av Spring Initializr

---
+++ src/main/kotlin/no/nav/pensjon/mq/sitm/Application.kt
@@ -0,0 +1,11 @@
+package no.nav.pensjon.mq.sitm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/no/nav/pensjon/mq/sitm/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.pensjon.mq.sitm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
